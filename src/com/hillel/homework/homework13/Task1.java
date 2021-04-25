8db40fc8409737a32bc344761d6ad45ac1968a0f
olober1337 <vadim.sukhetskii@gmail.com>
2021-04-25 21:43:16 +0300

    Homework13 50%

--- src/com/hillel/homework/homework13/Task1.java
+++ src/com/hillel/homework/homework13/Task1.java
@@ -13,49 +13,48 @@ public class Task1 {
 
     public static String pathName = "C:\\Users\\mcram\\Desktop\\Homework12\\myTextFile.txt";
 
-    public static HashMap<String, String> myHashMap = new LinkedHashMap<>();
+    public static HashMap<String, HashMap<String, String>> myLinkedHashMap = new LinkedHashMap<>();
+    public static HashMap<String, String> myLinkedHashMap2 = new LinkedHashMap<>();
+
 
     public static void main(String[] args) throws IOException {
 
-        questionsAndAnswers(myHashMap);
+        questionsAndAnswers(myLinkedHashMap);
 
     }
 
-    public static void questionsAndAnswers (HashMap<String, String> myHashMap) throws IOException {
+    public static void questionsAndAnswers (HashMap<String, HashMap<String, String>> myLinkedHashMap) throws IOException {
 
-        String key_1 = "Enter your name please :";
-        System.out.println(key_1);
-        String answer1 = answer.nextLine().concat("\n");
+        String question1 = "Enter your name please : ";
+        System.out.println(question1);
+        String answer1 = answer.nextLine();
 
-        String key_2 = "How are you feeling today? :";
-        System.out.println(key_2);
-        String answer2 = answer.nextLine().concat("\n");
+        String question2 = "How are you feeling today? : ";
+        System.out.println(question2);
+        String answer2 = answer.nextLine();
 
-        String key_3 = "Have you smiled today? :";
-        System.out.println(key_3);
-        String answer3 = answer.nextLine().concat("\n");
+        String question3 = "Have you smiled today? : ";
+        System.out.println(question3);
+        String answer3 = answer.nextLine();
 
-        String key_4 = "What is your country of residence? :";
-        System.out.println(key_4);
+        String question4 = "What is your country of residence? : ";
+        System.out.println(question4);
         String answer4 = answer.nextLine();
 
-        myHashMap.put(key_1, answer1);
-        myHashMap.put(key_2, answer2);
-        myHashMap.put(key_3, answer3);
-        myHashMap.put(key_4, answer4);
+        myLinkedHashMap2.put(question2, answer2);
+        myLinkedHashMap2.put(question3, answer3);
+        myLinkedHashMap2.put(question4, answer4);
+
+        myLinkedHashMap.put(answer1, myLinkedHashMap2);
 
         answer.close();
 
         File myTextFile = new File(pathName);
-
         FileWriter hashMapFileWriter = new FileWriter(myTextFile);
-
-        hashMapFileWriter.write(String.valueOf(myHashMap));
+        hashMapFileWriter.write(String.valueOf(myLinkedHashMap));
+        hashMapFileWriter.close();
 
         System.out.println("The parameters have been written to the file!!!");
 
-        hashMapFileWriter.close();
-
     }
-
 }
