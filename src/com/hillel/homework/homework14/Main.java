d9832660646b6af60c10eb81893a84d47ec272a5
olober1337 <vadim.sukhetskii@gmail.com>
2021-04-26 23:05:00 +0300

    Homework14 finished

--- src/com/hillel/homework/homework14/Main.java
+++ src/com/hillel/homework/homework14/Main.java
@@ -6,6 +6,15 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
+        /*Используя методы второго класса(промежуточного), в классе Main реализуйте методы использования следующих конструкций обработки ошибок:
+        а) try-catch-finally;
+        б) try-catch-catch-finally;
+        в) try-catch-catch-catch-finally;
+        г) try(with resources)-catch-finally;
+        д) try-finally (в каждой конструкции try должны быть вызовы 3х методов промежуточного класса с разными ошибками).*/
+
+
+
         MyMiddleClassForExceptions myMiddleClassForExceptions = new MyMiddleClassForExceptions();
 
         try {
