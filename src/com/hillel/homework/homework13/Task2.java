6a32a378e1c73f3a7f0fa5d918521b7d5b63b273
olober1337 <vadim.sukhetskii@gmail.com>
2021-04-28 18:37:45 +0300

    First course is finished

--- src/com/hillel/homework/homework13/Task2.java
+++ src/com/hillel/homework/homework13/Task2.java
@@ -7,8 +7,11 @@ public class Task2 {
     public static void main(String[] args) {
 
         List<Class1> newList1 = new LinkedList<>();
+        newList1.add(new Class1());
         List<Class2> newList2 = new LinkedList<>();
+        newList2.add(new Class2());
         List<Class3> newList3 = new LinkedList<>();
+        newList3.add(new Class3());
 
         wildcards(newList3, newList1);
 
@@ -16,7 +19,7 @@ public class Task2 {
 
     public static void wildcards (List<? extends Class3> list1, List<? super Class1> list2) {
 
-
+        list2.addAll(list1);
 
     }
 }
